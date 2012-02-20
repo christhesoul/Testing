7b366862ea215fea368f416ee54ea5954001e145
phpfog <git@phpfog.com>
2012-02-20 01:33:53 +0000

    Setup cellar27.co.uk

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'cellar27_co_uk');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'condiment-29138');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'L1M3p1ckl3!');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql-shared-02.phpfog.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         '0.294776884508234');
+define('SECURE_AUTH_KEY',  '0.294776884508234');
+define('LOGGED_IN_KEY',    '0.294776884508234');
+define('NONCE_KEY',        '0.294776884508234');
+define('AUTH_SALT',        '0.294776884508234');
+define('SECURE_AUTH_SALT', '0.294776884508234');
+define('LOGGED_IN_SALT',   '0.294776884508234');
+define('NONCE_SALT',       '0.294776884508234');
 
 /**#@-*/
 
